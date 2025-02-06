0b402943aee10c6b6d64a53148b558a6e3ee0a12
Vishhvajit <vishvajitgarud002@gmail.com>
2025-02-06 19:24:25 +0530

    Initial commit - Angular Reactive Forms project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, Validators,ReactiveFormsModule, AbstractControl, ValidationErrors} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'reactiveForm';
+  
+  registerForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    acceptTerms: new FormControl(false, [Validators.requiredTrue])
+  }, { validators: this.passwordMatchValidator });
+
+  //I used custom validator confirmpassword.
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
+
+  get fullName() { return this.registerForm.get('fullName'); }
+  get username() { return this.registerForm.get('username'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
+  get acceptTerms() { return this.registerForm.get('acceptTerms'); }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Registered User:', this.registerForm.value);
+      this.registerForm.reset();
+    }
+  }
+
+  onReset() {
+    this.registerForm.reset();
+  }
+
+
 }
