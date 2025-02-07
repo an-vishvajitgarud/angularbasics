0cfabd4b05eb5759161ff58442428ea923213879
an-vishvajitgarud <vishvajit.g@applicationnexus.com>
2025-02-07 12:52:46 +0530

    Refactor style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,6 @@ export class AppComponent {
   passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
-
     return password === confirmPassword ? null : { passwordsMismatch: true };
   }
 
